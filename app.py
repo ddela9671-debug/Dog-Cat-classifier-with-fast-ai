674a02b6f9bfe7ab83e683954374127dc8524c82
Daniel de la Pezuela <ddela9671@gmail.com>
2026-07-07 19:50:35 +0200

    Update app

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import gradio as gr
+from fastai.vision.all import *
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+learn = load_learner("model.pkl")
 
-def greet(name):
-    return "Hello " + name + "!!"
+categories=('Dog','Cat')
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+def classify_image(img):
+    pred, pred_idx, probs = learn.predict(img)
+    return {
+        learn.dls.vocab[i]: float(probs[i])
+        for i in range(len(probs))
+    }
+
+image = gr.Image(type="pil")
+label = gr.Label()
+examples = ['dog.jpg', 'gato.jpg', 'gorilla.webp','leon.jpg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
